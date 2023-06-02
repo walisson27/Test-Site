c56bd0566553b901332db38c4e2092f81d941909
Walisson Souza <walisson_souza7@hotmail.com>
2023-06-02 12:16:48 -0300

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
       <header className='border-b-4'>
         <section className='p-10 flex justify-center items-center'>
           <div>
-            <img className='w-24 md:w-full' src='./src/assets/logo.png' alt='Blocks logo' />
+            <img className='w-24 md:w-full' src='./assets/logo.png' alt='Blocks logo' />
           </div>
         </section>
         <hr className='colorfulCatalog w-full' />
